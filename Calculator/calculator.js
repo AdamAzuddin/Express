0d526f3aec388655862c03a06bd3eacd429ccf64
AdamAzuddin <05adamaz@gmail.com>
2023-04-29 15:41:41 +0800

    done

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -6,14 +6,17 @@ app.use(bodyParser.urlencoded({extended:true}))
 app.get("/", function (req,res){
     // name of file no matter the server deploy in cloud, local computer etc
     //__dirname give current path to this file no matter computer
-    res.sendFile(__dirname+"/index.html")
+    res.sendFile(__dirname+"/bmiCalculator.html")
 })
 
 app.post("/", function (req,res){
-    var num1 = Number(req.body.num1)
-    var num2 = Number(req.body.num2)
+    var height = Number(req.body.height)
+    var weight = Number(req.body.weight)
 
-    var result = num1+num2
-    res.send(`${num1}+${num2} = ${result}`)
+    var result = height*height
+    result= weight/height
+    result = result.toFixed(2)
+    
+    res.send(`Your BMI is ${result}`)
 })
 app.listen(3000, function(){console.log("Starting server at port 3000")})
