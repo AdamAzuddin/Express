1e7ad77170da6d1ca32f4529a1fab3b70dabe5f8
AdamAzuddin <05adamaz@gmail.com>
2023-04-29 10:03:24 +0800

    first commit

--- Express/server.js
+++ Express/server.js
@@ -8,4 +8,11 @@ app.get("/", function (req, res){
     // send this as reponse
     res.send("<h1>Hello from my first express server</h1>")
 })
+
+app.get("/contact", function (req, res){
+    res.send("azuddinadam@gmail.com")
+})
+app.get("/about", function (req, res){
+    res.send("Owned by Adam")
+})
 app.listen(3000, function(){console.log("Server has started at port 3000")})
