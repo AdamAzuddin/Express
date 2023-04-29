2e961997d5b9c6ce6d978a07589c7abc44ae0eaf
AdamAzuddin <05adamaz@gmail.com>
2023-04-29 09:36:34 +0800

    console log request

--- Express/server.js
+++ Express/server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express()
 
-
+// what happen when someone do request to home root
+app.get("/", function (request, response){
+    console.log(request)
+})
 app.listen(3000, function(){console.log("Server has started at port 3000")})
