5206b8341eec3fdebb71b57cd2f64df742f617bc
AdamAzuddin <05adamaz@gmail.com>
2023-04-29 09:47:05 +0800

    added comments

--- Express/server.js
+++ Express/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express()
 
-// what happen when someone do request to home root
+//when server get req to home route
 app.get("/", function (req, res){
+    
     console.log(req)
+    // send this as reponse
     res.send("<h1>Hello from my first express server</h1>")
 })
 app.listen(3000, function(){console.log("Server has started at port 3000")})
