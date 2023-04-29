e55565161a212deaa2c1e0d72c6a22313ca935df
AdamAzuddin <05adamaz@gmail.com>
2023-04-29 10:28:41 +0800

    undo es6 modules

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,8 +1,9 @@
-import express from "express"
-
+const express = require("express")
 const app = express()
 
 app.get("/", function (req,res){
-    res.send("Hello there!")
+    // name of file no matter the server deploy in cloud, local computer etc
+    //__dirname give current path to this file no matter computer
+    res.sendFile(__dirname+"/index.html")
 })
 app.listen(3000, function(){console.log("Starting server at port 3000")})
