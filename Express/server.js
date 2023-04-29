9523f1924bcdedeac44f86a78b148089751a8f5f
AdamAzuddin <05adamaz@gmail.com>
2023-04-29 09:43:49 +0800

    changed params name to req and res

--- Express/server.js
+++ Express/server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express()
 
 // what happen when someone do request to home root
-app.get("/", function (request, response){
-    console.log(request)
-    response.send("<h1>Hello from my first express server</h1>")
+app.get("/", function (req, res){
+    console.log(req)
+    res.send("<h1>Hello from my first express server</h1>")
 })
 app.listen(3000, function(){console.log("Server has started at port 3000")})
