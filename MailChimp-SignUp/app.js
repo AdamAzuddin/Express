5d151efd89cf84c4e3c5bb79da5fad554b99333b
AdamAzuddin <05adamaz@gmail.com>
2023-04-30 15:46:27 +0800

    get data

--- MailChimp-SignUp/app.js
+++ MailChimp-SignUp/app.js
@@ -5,21 +5,24 @@ const app = express();
 const bodyParser = require("body-parser");
 const request = require("request");
 
+
+app.use(bodyParser.urlencoded({extended:true}))
 app.use(express.static("public"))
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/signUp.html");
-});
+
 
 app.use(express.static("public"));
 
-// Set Content-Security-Policy header to disable strict MIME type checking
-app.use((req, res, next) => {
-  res.setHeader(
-    "Content-Security-Policy",
-    "default-src 'self' data: blob: 'unsafe-inline'"
-  );
-  return next();
-});
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/signUp.html");
+  });
+
+app.post("/", function (req,res){
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const email = req.body.email
+    console.log(firstName, lastName, email)
+})
 app.listen(3000, function () {
   console.log("Listening at port 3000");
 });
