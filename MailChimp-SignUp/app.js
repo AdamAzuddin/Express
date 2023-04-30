00be4cc24d5b4c97f1deffc596345155e36e8ae3
AdamAzuddin <05adamaz@gmail.com>
2023-04-30 15:39:30 +0800

    added styles.css

--- MailChimp-SignUp/app.js
+++ MailChimp-SignUp/app.js
@@ -1,9 +1,25 @@
-const express = require("express")
-const app = express()
-const bodyParser = require("body-parser")
+// jshint esversion: 6
 
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const request = require("request");
 
+app.use(express.static("public"))
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/signUp.html");
+});
 
-app.listen(3000, function(){
-    console.log("Listening at port 3000")
-})
+app.use(express.static("public"));
+
+// Set Content-Security-Policy header to disable strict MIME type checking
+app.use((req, res, next) => {
+  res.setHeader(
+    "Content-Security-Policy",
+    "default-src 'self' data: blob: 'unsafe-inline'"
+  );
+  return next();
+});
+app.listen(3000, function () {
+  console.log("Listening at port 3000");
+});
